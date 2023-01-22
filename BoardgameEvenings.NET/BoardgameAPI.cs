52706acb0b8501ec13de0e9b8174d4edf943ab88
Joel Blomberg <joel.blomberg@iths.se>
2023-01-22 11:12:13 +0100

    ...

---
+++ BoardgameEvenings.NET/BoardgameAPI.cs
@@ -0,0 +1,77 @@
+﻿using RestSharp;
+// ReSharper disable MemberCanBeMadeStatic.Global
+// ReSharper disable ClassNeverInstantiated.Global
+
+namespace BoardgameEvenings.NET;
+
+public record Event(int ID, string Date, string Name);
+
+public record Attendee(string Name, string Vote);
+
+public class BoardgameAPI
+{
+    public BoardgameAPI()
+    {
+        Console.WriteLine("API");
+    }
+
+    private static readonly RestClient _client = new("http://localhost:3000");
+
+    public async Task<List<Event>?> GetEvents() => (await _client.GetAsync<Event[]>(new("/events")))?.ToList();
+    
+    public async Task<Event?> GetEventById(int id)
+    {
+        RestRequest req = new("/event/{id}");
+        req.AddUrlSegment("id", id);
+
+        return await _client.GetAsync<Event>(req);
+    }
+
+    public async Task CreateEvent(Event gameEvent)
+    {
+        RestRequest req = new("/event", Method.Post);
+        req.AddJsonBody(gameEvent);
+
+        RestResponse res = await _client.PostAsync(req);
+        if (!res.IsSuccessful) throw new (res.ErrorMessage);
+    }
+
+    public async Task UpdateEvent(int id, Event gameEvent)
+    {
+        RestRequest req = new("/event/{id}", Method.Put);
+        req.AddUrlSegment("id", id);
+        req.AddJsonBody(gameEvent);
+
+        RestResponse res = await _client.PutAsync(req);
+        if (!res.IsSuccessful) throw new(res.ErrorMessage);
+    }
+
+    public async Task DeleteEvent(int id)
+    {
+        RestRequest req = new("/event/{id}", Method.Delete);
+        req.AddUrlSegment("id", id);
+
+        RestResponse res = await _client.DeleteAsync(req);
+        if (!res.IsSuccessful) throw new(res.ErrorMessage);
+    }
+
+    public async Task AttendEvent(int id, Attendee attendee)
+    {
+        RestRequest req = new("/event/:id/attend", Method.Post);
+        req.AddUrlSegment("id", id);
+        req.AddJsonBody(attendee);
+
+        RestResponse res = await _client.PostAsync(req);
+        if (!res.IsSuccessful) throw new(res.ErrorMessage);
+    }
+
+    public async Task RemoveAttendance(int EventId, int AttendeeId)
+    {
+        RestRequest req = new("/event/:eventId/attend/:attendeeId", Method.Delete);
+        req.AddUrlSegment("eventId", EventId);
+        req.AddUrlSegment("attendeeId", AttendeeId);
+
+        RestResponse res = await _client.DeleteAsync(req);
+        if (!res.IsSuccessful) throw new(res.ErrorMessage);
+    }
+}
--- BoardgameEvenings.NET/Program.cs
+++ BoardgameEvenings.NET/Program.cs
@@ -1,13 +1,14 @@
-var builder = WebApplication.CreateBuilder(args);
+using BoardgameEvenings.NET;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+builder.Services.AddSingleton<BoardgameAPI>();
 
-// Configure the HTTP request pipeline.
+WebApplication app = builder.Build();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -16,9 +17,95 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/", () =>
+app.MapGet("/", async (BoardgameAPI api) =>
 {
-    Results.Ok();
+    try
+    {
+        List<Event>? e = await api.GetEvents();
+        return e?.Count > 0 ? Results.Ok(e) : Results.NoContent();
+    }
+    catch (Exception ex)
+    {
+        return Results.BadRequest(ex.Message);
+    }
+});
+
+app.MapGet("/event/{id:int}", async (BoardgameAPI api, int id) =>
+{
+    try
+    {
+        Event? gameEvent = await api.GetEventById(id);
+        return gameEvent != null ? Results.Ok(gameEvent) : Results.NotFound();
+    }
+    catch (Exception ex)
+    {
+        return Results.BadRequest(ex.Message);
+    }
+});
+
+app.MapPost("/event", async (BoardgameAPI api, Event gameEvent) =>
+{
+    try
+    {
+        await api.CreateEvent(gameEvent);
+        return Results.Ok();
+    }
+    catch (Exception ex)
+    {
+        return Results.BadRequest(ex.Message);
+    }
+});
+
+app.MapPut("/event/{id:int}", async (BoardgameAPI api, int id, Event gameEvent) =>
+{
+    try
+    {
+        await api.UpdateEvent(id, gameEvent);
+        return Results.Ok();
+    }
+    catch (Exception ex)
+    {
+        return Results.BadRequest(ex.Message);
+    }
+});
+
+app.MapDelete("/event{id:int}", async (BoardgameAPI api, int id) =>
+{
+    try
+    {
+        await api.DeleteEvent(id);
+        return Results.Ok();
+    }
+    catch (Exception ex)
+    {
+        return Results.BadRequest(ex.Message);
+    }
+});
+
+app.MapPost("/event{id:int}/attend", async (BoardgameAPI api, int id, Attendee attendee) =>
+{
+    try
+    {
+        await api.AttendEvent(id, attendee);
+        return Results.Ok();
+    }
+    catch (Exception ex)
+    {
+        return Results.BadRequest(ex.Message);
+    }
+});
+
+app.MapDelete("/event/{eventId:int}/attend/{attendeeId:int}", async (BoardgameAPI api, int eventId, int attendeeId) =>
+{
+    try
+    {
+        await api.RemoveAttendance(eventId, attendeeId);
+        return Results.Ok();
+    }
+    catch (Exception ex)
+    {
+        return Results.BadRequest(ex.Message);
+    }
 });
 
 app.Run();
